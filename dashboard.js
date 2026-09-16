a33af263bcb8c0c2ee0c6c4cc037357428a9a560
Endless <endlessnewslk@gmail.com>
2026-09-16 18:00:37 +0300

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1111,7 +1111,7 @@ async function saveNewsItem() {
             adminNews[idx] = Object.assign({}, adminNews[idx], newsItem, { id: editingNewsId });
         }
     } else {
-        adminNews.unshift(newsItem);
+        adminNews.push(newsItem);
     }
 
     saveNews();
